985b10a90e3d94ff3e3df8057791cb3e353a3c5c
Anurag Pratap Singh <23cseaiml012@jssaten.ac.in>
2025-01-24 17:33:16 +0530

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,15 +1,15 @@
 const btn = document.getElementById('emailbhejo');
+const form = document.querySelector('.subscribe-form'); // or use getElementById if you have an ID for the form
 
-document.getElementByClassName('subscribe-form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-   btn.value = 'Sending...';
+  btn.value = 'Sending...';
 
-   const serviceID = 'service_mzzk4o8';
-   const templateID = 'template_046438y';
+  const serviceID = 'service_mzzk4o8';
+  const templateID = 'template_046438y';
 
-   emailjs.sendForm(serviceID, templateID, this)
+  emailjs.sendForm(serviceID, templateID, form)
     .then(() => {
       btn.value = 'Send Email';
       alert('Sent!');
