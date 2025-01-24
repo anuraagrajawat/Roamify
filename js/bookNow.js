1781e2c447ed3169bc85204062f68278c45a27f1
Anurag Pratap Singh <23cseaiml012@jssaten.ac.in>
2025-01-25 01:57:33 +0530

    Update bookNow.js

--- js/bookNow.js
+++ js/bookNow.js
@@ -1,7 +1,10 @@
-// Initialize EmailJS with your user ID
-emailjs.init('4sO-R-TYpnLPdB5Gm'); // Replace with your actual user ID
+emailjs.init('4sO-R-TYpnLPdB5Gm');
+
 const btnn = document.getElementById('bookNowBtn');
 const formm = document.querySelector('#mail-form');
+
+const serviceID = 'service_mzzk4o8';
+const templateID = 'template_ud9vgw4';
 
 formm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -9,32 +12,16 @@ formm.addEventListener('submit', function(event) {
   btnn.disabled = true;
   btnn.innerText = 'Booking...';
 
-  const serviceID = 'service_mzzk4o8'; // Replace with your service ID
-  const templateID = 'template_ud9vgw4'; // Replace with your template ID
-
-  // Send the email using hardcoded values for testing
-  emailjs.send('service_mzzk4o8', 'template_ud9vgw4', {
-    user_name: 'Test User',
-    user_email: 'meritation99@gmail.com',
-    user_mobile: '1234567890',
-    user_package: 'Solo Traveler'
-  }).then(() => {
-    console.log('Email sent successfully!');
-  }, (err) => {
-    console.error('Error sending email:', err);
-  });
-
-  // Uncomment this block to send the actual form data once the test works
-  // emailjs.sendForm(serviceID, templateID, formm)
-  //   .then(() => {
-  //     btnn.disabled = false;
-  //     btnn.innerText = 'Booked!';
-  //     alert('Thank you for booking! We will get in touch with you shortly.');
-  //   })
-  //   .catch((err) => {
-  //     btnn.disabled = false;
-  //     btnn.innerText = 'BOOK NOW';
-  //     console.error('EmailJS Error:', err);
-  //     alert('Failed to send email. Please check the console for details.');
-  //   });
+  emailjs.sendForm(serviceID, templateID, formm)
+    .then(() => {
+      btnn.disabled = false;
+      btnn.innerText = 'Booked!';
+      alert('Thank you for booking! We will get in touch with you shortly.');
+    })
+    .catch((err) => {
+      btnn.disabled = false;
+      btnn.innerText = 'BOOK NOW';
+      console.error('EmailJS Error:', err);
+      alert('Failed to send email. Please check the console for details.');
+    });
 });
