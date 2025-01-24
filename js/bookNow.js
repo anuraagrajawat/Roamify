beff3dc7f12432f5fb24b06dd3c6a587e4a7a4fb
Anurag Pratap Singh <23cseaiml012@jssaten.ac.in>
2025-01-25 01:48:07 +0530

    Update bookNow.js

--- js/bookNow.js
+++ js/bookNow.js
@@ -4,14 +4,14 @@ emailjs.init('4sO-R-TYpnLPdB5Gm'); // Replace with your actual user ID
 // Enable debugging for more detailed error messages
 emailjs.debug(true); // This will log additional debug information to the console
 
-const btn = document.getElementById('bookNowBtn');
+const btnn = document.getElementById('bookNowBtn');
 const form = document.querySelector('#mail-form');
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  btn.disabled = true;
-  btn.innerText = 'Booking...';
+  btnn.disabled = true;
+  btnn.innerText = 'Booking...';
 
   const serviceID = 'service_mzzk4o8'; // Replace with your service ID
   const templateID = 'template_ud9vgw4'; // Replace with your template ID
@@ -31,13 +31,13 @@ form.addEventListener('submit', function(event) {
   // Uncomment this block to send the actual form data once the test works
   // emailjs.sendForm(serviceID, templateID, form)
   //   .then(() => {
-  //     btn.disabled = false;
-  //     btn.innerText = 'Booked!';
+  //     btnn.disabled = false;
+  //     btnn.innerText = 'Booked!';
   //     alert('Thank you for booking! We will get in touch with you shortly.');
   //   })
   //   .catch((err) => {
-  //     btn.disabled = false;
-  //     btn.innerText = 'BOOK NOW';
+  //     btnn.disabled = false;
+  //     btnn.innerText = 'BOOK NOW';
   //     console.error('EmailJS Error:', err);
   //     alert('Failed to send email. Please check the console for details.');
   //   });
